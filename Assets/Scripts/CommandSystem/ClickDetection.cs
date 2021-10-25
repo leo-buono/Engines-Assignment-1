1f5bed3436fd9a0955f8dd73d11149400725bf24
Jonathan Jay <jonathan.jay@ontariotech.net>
2021-10-25 02:15:38 -0400

    I did some thigns
    dont push github at 3AM

--- Assets/Scripts/CommandSystem/ClickDetection.cs
+++ Assets/Scripts/CommandSystem/ClickDetection.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(RectTransform))]
 public class ClickDetection : MonoBehaviour
 {
+	public RectTransform UItransform;
+	public int pixelHeight = 1080;
+	float camDistance = 15f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			Vector3 pos = Input.mousePosition;
+			pos.z = camDistance;
+			Collider2D touch = CollisionCheck(Camera.main.ScreenToWorldPoint(pos));
+			if (touch != null) {
+				Debug.Log(touch.name);
+			}
+		}
     }
+
+	//relative to Canvas data
+	Vector2 ConvertedMousePos()
+	{
+		Vector2 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		pos = pos * 2f - Vector2.one;
+		pos.y *= pixelHeight * 0.5f;
+		pos.x *= pixelHeight * 0.5f * Camera.main.aspect;
+		return pos;
+	}
+
+	Collider2D CollisionCheck(Vector2 input)
+	{
+		return Physics2D.OverlapPoint(input);
+	}
 }
